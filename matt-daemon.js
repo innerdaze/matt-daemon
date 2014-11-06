a93a52fd33950cb5a0d783c53a318868b369dd30
Lee Driscoll <lsdriscoll@gmail.com>
2014-11-06 14:01:13 +0000

    Added logging

--- matt-daemon.js
+++ matt-daemon.js
@@ -14,8 +14,35 @@
 var PORT = 8000;
 var SITE_DIRECTORY = 'app';
 
-var sys = require('sys');
+var util = require('util');
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout); }
-exec("http-server " + SITE_DIRECTORY + " -p " + PORT, puts);
+var fs = require('fs');
+var Log = require('log');
+//var debugLog = new Log('debug', fs.createWriteStream('matt-daemon.debug.log'));
+var errorLog = new Log('error', fs.createWriteStream('matt-daemon.error.log'));
+var infoLog = new Log('info', fs.createWriteStream('matt-daemon.log'));
+
+function puts(error, stdout, stderr) {
+
+    util.puts(stdout);
+    infoLog.info(stdout);
+
+    util.puts('stderr: ' + stderr);
+    errorLog.error(stderr);
+
+    if (error !== null) {
+        util.puts('exec error: ' + error);
+        errorLog.error(error);
+    }
+
+}
+
+var childProcess = exec("http-server " + SITE_DIRECTORY + " -p " + PORT, puts);
+
+childProcess.on('error', function(err){
+
+    errorLog.error(err);
+
+});
+
 require('daemon')();
