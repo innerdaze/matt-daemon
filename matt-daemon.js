5f89a9d79715ada880ba8cb63a8d263fd47413c7
Lee Driscoll <lsdriscoll@gmail.com>
2014-11-06 17:01:04 +0000

    Matt Daemon now restarts the HTTP listener if the daemon is restarted

--- matt-daemon.js
+++ matt-daemon.js
@@ -11,38 +11,83 @@
  * @copyright       Fuck copyright
  *
  */
-var PORT = 8000;
-var SITE_DIRECTORY = 'app';
+module.exports = function(opts){
 
-var util = require('util');
-var exec = require('child_process').exec;
-var fs = require('fs');
-var Log = require('log');
-//var debugLog = new Log('debug', fs.createWriteStream('matt-daemon.debug.log'));
-var errorLog = new Log('error', fs.createWriteStream('matt-daemon.error.log'));
-var infoLog = new Log('info', fs.createWriteStream('matt-daemon.log'));
+    var util = require('util');
 
-function puts(error, stdout, stderr) {
-
-    util.puts(stdout);
-    infoLog.info(stdout);
-
-    util.puts('stderr: ' + stderr);
-    errorLog.error(stderr);
-
-    if (error !== null) {
-        util.puts('exec error: ' + error);
-        errorLog.error(error);
+    if(!opts){
+        util.puts('Matt Daemon can\'t guess your config, so help a chap out.');
+        return;
     }
 
-}
+    if(!opts.port){
+        util.puts('Matt Daemon would love to serve, but he needs to know what port you want to publish on.');
+        return;
+    }
 
-var childProcess = exec("http-server " + SITE_DIRECTORY + " -p " + PORT, puts);
+    if(!opts.root){
+        util.puts('Matt Daemon has almost figured it out, but he needs to know which folder to serve up.');
+        return;
+    }
 
-childProcess.on('error', function(err){
+    var PORT = opts.port;
+    var SITE_DIRECTORY = opts.root;
 
-    errorLog.error(err);
+    var kill = require('./lib/kill-child-process').kill;
 
-});
+    var exec = require('child_process').exec;
+    var fs = require('fs');
+    var path = require('path');
+    var Log = require('log');
+    //var debugLog = new Log('debug', fs.createWriteStream('matt-daemon.debug.log'));
+    var errorLog = new Log('error', fs.createWriteStream('matt-daemon.error.log'));
+    var infoLog = new Log('info', fs.createWriteStream('matt-daemon.log'));
+    //var pidStream = fs.createWriteStream('matt-daemon.pid');
 
-require('daemon')();
+
+    var pidFilePath = 'matt-daemon.pid';
+
+    // First let's check for existing instance and kill it off
+    if(fs.existsSync(pidFilePath)) {
+        var pid = fs.readFileSync(pidFilePath);
+        if (pid.length && pid !== process.pid) {
+            util.puts('Killing process with id: ' + pid);
+            process.kill(pid, 'SIGHUP');
+            fs.writeFile(pidFilePath, '');
+        }
+    }
+
+    // exec callback
+    function puts(error, stdout, stderr) {
+
+        util.puts(stdout);
+        infoLog.info(stdout);
+
+        util.puts('stderr: ' + stderr);
+        errorLog.error(stderr);
+
+        if (error !== null) {
+            util.puts('exec error: ' + error);
+            errorLog.error(error);
+        }
+
+    }
+
+    util.puts('Starting Matt Daemon Server');
+
+
+    var childProcess = exec("./node_modules/.bin/http-server " + SITE_DIRECTORY + " -p " + PORT, {
+        cwd: __dirname
+    }, puts);
+
+    childProcess.on('error', function(err){
+        errorLog.error(err);
+    });
+
+    fs.writeFile(pidFilePath, childProcess.pid.toString());
+
+    infoLog.info('Starting Matt Daemon Server with pid: ' + childProcess.pid);
+
+    require('daemon')();
+
+};
