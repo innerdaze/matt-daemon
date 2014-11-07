628c80cfd581b48b363351b190e190ab3522f489
Lee Driscoll <lsdriscoll@gmail.com>
2014-11-07 09:59:51 +0000

    Matt Daemon now pipes the http-server output streams into the log files

--- matt-daemon.js
+++ matt-daemon.js
@@ -40,8 +40,6 @@ module.exports = function(opts){
     //var debugLog = new Log('debug', fs.createWriteStream('matt-daemon.debug.log'));
     var errorLog = new Log('error', fs.createWriteStream('matt-daemon.error.log'));
     var infoLog = new Log('info', fs.createWriteStream('matt-daemon.log'));
-    //var pidStream = fs.createWriteStream('matt-daemon.pid');
-
 
     var pidFilePath = 'matt-daemon.pid';
 
@@ -55,36 +53,48 @@ module.exports = function(opts){
         }
     }
 
-    // exec callback
+    // Function to call when HTTP Server exits
     function puts(error, stdout, stderr) {
 
-        util.puts(stdout);
         infoLog.info(stdout);
 
-        util.puts('stderr: ' + stderr);
         errorLog.error(stderr);
 
         if (error !== null) {
-            util.puts('exec error: ' + error);
             errorLog.error(error);
         }
 
     }
 
-    util.puts('Starting Matt Daemon Server');
-
-    var childProcess = exec("./node_modules/.bin/http-server " + SITE_DIRECTORY + " -p " + PORT, {
+    // Spawn HTTP server
+    var childProcess = exec('./node_modules/.bin/http-server ' + SITE_DIRECTORY + ' -p ' + PORT, {
         cwd: __dirname
     }, puts);
 
-    childProcess.on('error', function(err){
-        errorLog.error(err);
+    // Stream outputs to log files
+    childProcess.on('error', errorLog.error.bind(errorLog));
+
+    childProcess.stderr.on('data', errorLog.error.bind(errorLog));
+
+    childProcess.stdout.on('data', function(data){
+        infoLog.info(data.toString());
     });
 
+    // Write PID to file
     fs.writeFile(pidFilePath, childProcess.pid.toString());
 
-    infoLog.info('Starting Matt Daemon Server with pid: ' + childProcess.pid);
+    util.puts('Starting Matt Daemon Server | PID: ' + childProcess.pid + ' | PORT: ' + PORT + ' | ROOT: ' + SITE_DIRECTORY);
+    infoLog.info('Starting Matt Daemon Server | PID: ' + childProcess.pid + ' | PORT: ' + PORT + ' | ROOT: ' + SITE_DIRECTORY);
 
     require('daemon')();
 
 };
+
+/**
+ * Child_Process Events
+ * error
+ * exit
+ * close
+ * disconnect
+ * message
+ **/
